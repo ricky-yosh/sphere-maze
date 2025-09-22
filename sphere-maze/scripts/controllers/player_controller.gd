b67bddb60bf013f6c73ea79ca244a53303020828
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-09-21 20:02:40 -0700

    Organize code structure

--- sphere-maze/scripts/controllers/player_controller.gd
+++ sphere-maze/scripts/controllers/player_controller.gd
@@ -1,54 +1,56 @@
-# player_controller.gd
 extends RigidBody3D
 
-@export var movement_data: PlayerMovementData
-@export var camera_pivot_path: NodePath
+@export var player_movement_data: PlayerMovementRepository
+@export var camera_pivot: Node3D
 
-var camera_pivot: Node3D
+#region Virtual Methods
 
 func _ready() -> void:
-	camera_pivot = get_node(camera_pivot_path)
-	
-	# Create default movement data if not assigned
-	if not movement_data:
-		movement_data = PlayerMovementData.new()
+	_restore_movement_data()
 
 func _physics_process(delta: float) -> void:
-	# Check if grounded
-	var is_grounded = PlayerMovementSystem.check_ground_collision(self)
-	movement_data.set_grounded(is_grounded)
-	
-	# Get input direction
-	var input_dir = _get_input_direction()
-	
-	# Calculate movement direction relative to camera
-	var cam_basis = camera_pivot.global_transform.basis
-	var move_dir = PlayerMovementSystem.get_movement_direction(input_dir, cam_basis)
-	
-	# Apply movement
-	PlayerMovementSystem.apply_movement(self, move_dir, movement_data)
-	
-	# Handle jumping
-	if Input.is_action_just_pressed("jump"):
-		PlayerMovementSystem.apply_jump(self, movement_data)
-	
-	# Allow for exiting the game
-	if Input.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	# Make the pivot follow ball's position (no rotation!)
-	camera_pivot.global_position = global_position
+	_capture_player_movement_inputs()
+	_capture_jump_input()
+	_capture_menu_input()
+	_update_camera_pivot()
 
-func _get_input_direction() -> Vector3:
-	var input_dir = Vector3.ZERO
+#endregion
+
+#region Private Methods
+
+func _restore_movement_data() -> void:
+	if not player_movement_data:
+		player_movement_data = PlayerMovementRepository.new()
+
+## Captures and processes player movement input.
+func _capture_player_movement_inputs() -> void:
+	var input_vector = Vector3()
 	
 	if Input.is_action_pressed("forward"):
-		input_dir.z -= 1
+		input_vector.z -= 1
 	if Input.is_action_pressed("back"):
-		input_dir.z += 1
+		input_vector.z += 1
 	if Input.is_action_pressed("left"):
-		input_dir.x -= 1
+		input_vector.x -= 1
 	if Input.is_action_pressed("right"):
-		input_dir.x += 1
-	
-	return input_dir
+		input_vector.x += 1
+		
+	PlayerMovementService.handle_movement(self, input_vector, player_movement_data)
+
+## Captures and processes jump input.
+func _capture_jump_input() -> void:
+	if Input.is_action_just_pressed("jump"):
+		PlayerMovementService.handle_jump(self, player_movement_data)
+
+## Captures menu input and allows user to use mouse.
+func _capture_menu_input() -> void:
+	if Input.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+
+func _update_camera_pivot() -> void:
+	if camera_pivot:
+		# Follow the player, no rotation
+		camera_pivot.global_position = global_position
+		
+#endregion
---
+++ sphere-maze/scripts/repository/player_movement_repository.gd
@@ -0,0 +1,6 @@
+class_name PlayerMovementRepository
+extends Resource
+
+@export var move_speed: float = 20.0
+@export var jump_force: float = 5.0
+@export var max_jumps: int = 1
